4f85d059674e130acc348ea0e36a847642b91902
Alexis Aneas <altair.alek@gmail.com>
2022-04-29 11:57:24 +0200

    kotlin migration

---
+++ app/src/main/java/com/openclassrooms/realestatemanager/SecondActivity.kt
@@ -0,0 +1,14 @@
+package com.openclassrooms.realestatemanager
+
+import android.net.wifi.WifiManager
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.TextView
+import android.os.Bundle
+import com.openclassrooms.realestatemanager.R
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
